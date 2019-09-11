4f9707fb69c19bb2c5213e0713caaa90df757655
Bryan Roberto Caceres Toribio <bryan.caceres@utec.edu.pe>
2019-09-11 17:04:56 -0500

    punteros

---
+++ session4a/cmake-build-debug/CMakeFiles/session4a.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session4a.dir/main.cpp.obj"
+  "session4a.pdb"
+  "session4a.exe"
+  "session4a.exe.manifest"
+  "session4a.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session4a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
