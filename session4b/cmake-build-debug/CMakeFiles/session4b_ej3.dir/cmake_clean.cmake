3a6fce754381cb822a84e2bc9b7ca46da92bd9f3
Seyrul <54951315+Seyrul@users.noreply.github.com>
2019-09-12 10:38:04 -0500

    Ejercicio 2 con punteros

---
+++ session4b/cmake-build-debug/CMakeFiles/session4b_ej3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session4b_ej3.dir/ejercicio3.cpp.obj"
+  "session4b_ej3.pdb"
+  "session4b_ej3.exe"
+  "session4b_ej3.exe.manifest"
+  "session4b_ej3.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session4b_ej3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ session4b/cmake-build-debug/CMakeFiles/session4b_ej4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/session4b_ej4.dir/ejercicio4.cpp.obj"
+  "session4b_ej4.pdb"
+  "session4b_ej4.exe"
+  "session4b_ej4.exe.manifest"
+  "session4b_ej4.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/session4b_ej4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
